41d407378e0ff243355a87ac009d35702bf8615d
Raymond BBF <ketchup942@gmail.com>
2021-03-08 18:17:09 +0800

    'update'

--- app.py
+++ app.py
@@ -40,9 +40,20 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+
+    if msg in ['yoyo', 'hi']:
+        x = '你好'
+    elif msg == '你吃飽了嗎':
+        x = '還沒'
+    elif msg == '你是誰':
+        x = '我是機器人'
+    elif '訂位' in msg:
+        x = '你想訂位,是嗎?'
+        
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=x))
 
 
 if __name__ == "__main__":
